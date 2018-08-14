31d2255abe07f8ea74805d72d85f04107eadb9a1
windnc <windnc@gmail.com>
2018-08-14 17:55:17 +0900

    is_empty_dir

--- util/fs.py
+++ util/fs.py
@@ -14,9 +14,9 @@ if sys.version_info<(3,0,0):
     sys.setdefaultencoding('utf8')
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~5~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
-def is_movie( fn ):
+def is_movie(fn):
     if not fn: return False
-    ext = os.path.splitext( fn )[1].lower()
+    ext = os.path.splitext(fn)[1].lower()
     if ext == ".mp4": return True
     elif ext == ".avi": return True
     elif ext == ".mkv": return True
@@ -24,14 +24,14 @@ def is_movie( fn ):
     return False
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
-def check_filter( filter_dict, path ):
+def check_filter(filter_dict, path):
     if not filter_dict: return True
     for k, v in filter_dict.items():
         if k == "ext":
             return True
         elif k == "contain":
             for patt in v:
-                if path.find( patt ) < 0: return False
+                if path.find(patt) < 0: return False
 
     return True
 
@@ -65,8 +65,8 @@ class FileInfo:
         self.ext = e
         self.cstamp = int(os.path.getctime(full_fn))
         self.mstamp = int(os.path.getmtime(full_fn))
-        self.cstr = datetime.datetime.fromtimestamp( self.cstamp ).strftime("%y.%m.%d %H:%M:%S")
-        self.mstr = datetime.datetime.fromtimestamp( self.mstamp ).strftime("%y.%m.%d %H:%M:%S")
+        self.cstr = datetime.datetime.fromtimestamp(self.cstamp).strftime("%y.%m.%d %H:%M:%S")
+        self.mstr = datetime.datetime.fromtimestamp(self.mstamp).strftime("%y.%m.%d %H:%M:%S")
         self.size = os.path.getsize(full_fn)
         self.sizestr = "{:,}".format(self.size)
         st = os.stat(full_fn)
@@ -74,14 +74,17 @@ class FileInfo:
         return
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
-def get_parent( root ):
+def get_parent(root):
     if root == "/":
         return None
     return os.path.abspath(os.path.join(root, os.pardir))
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
-def get_file_list( root, filter_dict=None, recursive=False):
+def get_file_list(root, filter_dict=None, recursive=False):
+    if not os.path.isdir(root):
+        return False
+
     if recursive is False:
         tmplist = list()
         for fn in os.listdir(root):
@@ -89,13 +92,12 @@ def get_file_list( root, filter_dict=None, recursive=False):
             if os.path.isfile(full_fn):
                 tmplist.append(FileInfo(root, fn))
         return tmplist
-
     else:
         fn_list = list()
-        for dn, dns, fns in os.walk( root ):
+        for dn, dns, fns in os.walk(root):
             for fn2 in fns:
-                if check_filter( filter_dict, fn2 ):
-                    fn_list.append( (os.path.join( dn, fn2 ),fn2) )
+                if check_filter(filter_dict, fn2):
+                    fn_list.append((os.path.join(dn, fn2),fn2))
         return fn_list
 
 
@@ -123,8 +125,8 @@ class DirInfo:
         self.path = path
         self.cstamp = int(os.path.getctime(full_dn))
         self.mstamp = int(os.path.getmtime(full_dn))
-        self.cstr = datetime.datetime.fromtimestamp( self.cstamp ).strftime("%y.%m.%d %H:%M:%S")
-        self.mstr = datetime.datetime.fromtimestamp( self.mstamp ).strftime("%y.%m.%d %H:%M:%S")
+        self.cstr = datetime.datetime.fromtimestamp(self.cstamp).strftime("%y.%m.%d %H:%M:%S")
+        self.mstr = datetime.datetime.fromtimestamp(self.mstamp).strftime("%y.%m.%d %H:%M:%S")
         #self.size = os.path.getsize(full_fn)
         self.size = 0
 
@@ -133,6 +135,9 @@ class DirInfo:
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
 def get_dir_list(root, recursive=False):
+    if not os.path.isdir(root):
+        return False
+
     if recursive is False:
         tmplist = list()
         for tmp in os.listdir(root):
@@ -143,34 +148,69 @@ def get_dir_list(root, recursive=False):
 
     else:
         dn_list = list()
-        for dn, dns, fns in os.walk( root ):
+        for dn, dns, fns in os.walk(root):
             for dn2 in dns:
-                dn_list.append( (os.path.join( dn, dn2 ), dn2) )
+                dn_list.append((os.path.join(dn, dn2), dn2))
         return dn_list
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~⏎
-def del_empty_dir( dn, verbose=False ):
+def is_empty_dir(root, verbose=1):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~⏎
+    if verbose >= 1:
+        print("is_empty_dir: %s" % (root))
+
+    file_list = get_file_list(root, recursive=False)
+    if file_list is False:
+        if verbose >= 2:
+            print( "false file_list: %s" % root)
+        return False
+    if len(file_list) > 0:
+        if verbose >= 2:
+            print( "file more than 0")
+        return False
+
+    dir_list = get_dir_list(root, recursive=False)
+    if dir_list is False:
+        if verbose >= 2:
+            print( "false dir_list: %s" % root)
+        return False
+    if len(dir_list) > 0:
+        if verbose >= 2:
+            print( "dir more than 0")
+        return False
+
+    return True
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~⏎
+def del_empty_dir(root, verbose=1):
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~⏎
+    if verbose >= 1:
+        print("del_empty_dir: %s" % (root))
+
+    if not is_empty_dir(root, verbose):
+        return True
+
     try:
-        if verbose: print( "delete %s" % (dn) )
-        os.rmdir( dn )
-    except:
-        if verbose: print( "delete error! %s" % (dn) )
+        os.rmdir(root)
+    except Exception as e:
+        if verbose:
+            print("delete error! %s" % (root))
+            print("%s" % (e))
         return False
     return True
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~⏎
-def del_file( fn, verbose=1):
+def del_file(fn, verbose=1):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~⏎
     if verbose >= 1:
-        print( "del_file %s" % (fn) )
+        print("del_file %s" % (fn))
 
     try:
-        os.remove( fn )
+        os.remove(fn)
     except Exception as e:
-        print( "delete error! %s" % (fn) )
-        print( e )
+        print("delete error! %s" % (fn))
+        print(e)
         return False
     return True
 
@@ -178,7 +218,7 @@ def del_file( fn, verbose=1):
 def assert_no_file(fn, verbose=1):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~⏎
     if verbose >= 1:
-        print( "assert no file %s" % (fn) )
+        print("assert no file %s" % (fn))
 
     if os.path.isfile(fn):
         print(verbose)
@@ -186,25 +226,25 @@ def assert_no_file(fn, verbose=1):
     return True
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
-def move_file( src, tgt, verbose=1 ):
+def move_file(src, tgt, verbose=1):
     try:
         if verbose >= 1:
-            print( "move %s -> %s" % (src,tgt) )
-        shutil.move( src, tgt )
+            print("move %s -> %s" % (src,tgt))
+        shutil.move(src, tgt)
     except Exception as e:
-        print( "error!" )
-        print( e )
+        print("error!")
+        print(e)
         return False
 
     return True
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~⏎
-def assert_dir( dn, verbose=1 ):
+def assert_dir(dn, verbose=1):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~⏎
-    if not os.path.exists( dn ):
+    if not os.path.exists(dn):
         if verbose>=1:
-            print( "mkdir %s" % (dn) )
+            print("mkdir %s" % (dn))
 
         os.makedirs(dn)
 
@@ -215,23 +255,23 @@ def assert_dir( dn, verbose=1 ):
         if e.errno != errno.EEXIST:
             raise
     """
-    return os.path.exists( dn )
+    return os.path.exists(dn)
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
-def txtfile2dict( fn ):
+def txtfile2dict(fn):
     d = dict()
-    with open( fn,'r') as f:
+    with open(fn,'r') as f:
         for line in f.readlines():
-            line = line.strip( "\n" )
+            line = line.strip("\n")
             d[ line ] = True
     return d
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
-def tsvfile2dict( fn, keyid, valueid ):
+def tsvfile2dict(fn, keyid, valueid):
     d = dict()
-    with open( fn,'r') as f:
+    with open(fn,'r') as f:
         for line in f.readlines():
             line = line.strip("\n")
             arr = line.split("\t")
@@ -241,8 +281,8 @@ def tsvfile2dict( fn, keyid, valueid ):
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
 def test():
-    if len( sys.argv ) != 3:
-        print( "usage: %s fn-in fn-out" % sys.argv[0] )
+    if len(sys.argv) != 3:
+        print("usage: %s fn-in fn-out" % sys.argv[0])
         return False
     return True
 
