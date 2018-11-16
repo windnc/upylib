a4e7f13bf0c96c960397b79f813b7958b2585c2e
windnc <windnc@gmail.com>
2018-11-16 14:20:52 +0900

    add

---
+++ upylib/util/regex.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-⏎
+from __future__ import print_function
+import sys
+import re
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~5~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
+if sys.version_info<(3,0,0):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~5~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
+def regex_replace(s, patt, rep):
+    res = re.search(patt, s)
+    if res is None:
+        return s
+
+    s = rep
+    for i in range(1,9):
+        var = "${%d}" % i
+        if var not in s:
+            break
+        s = s.replace( "${%d}" % i, res.group(i) )
+
+    return s
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
+def test():
+    """
+    if len(sys.argv) != 3:
+        print("usage: %s fn-in fn-out" % sys.argv[0])
+        return False
+    """
+    s = "/banana/apple/cup (2018)/"
+    print(s)
+
+    patt = "/(.*)(.*)"
+    print(patt)
+
+    out = regex_replace(s, patt, "${1}\n${2}")
+    print(out)
+
+    patt = "/(.*)(\\d{4})"
+    out = regex_replace(s, patt, "${1}\n${2}")
+    print(out)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
+if __name__ == "__main__":
+    test()
+
