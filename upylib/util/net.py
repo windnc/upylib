06b050c37de2e7257509df81e4639191918494ca
windnc <windnc@gmail.com>
2018-12-07 21:04:32 +0900

    fix

--- upylib/util/net.py
+++ upylib/util/net.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-⏎
 
 from __future__ import print_function
@@ -13,13 +13,17 @@ if sys.version_info<(3,0,0):
     sys.setdefaultencoding('utf8')
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~5~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
-def read_url(url, retry=1):
+def read_url(url, retry=1, encoding="utf-8"):
     if not url: return False
 
     for _ in range(0,retry):
         try:
             c = urlreq.urlopen(url)
-            return c.read()
+            content = c.read()
+            if encoding != None:
+                content = content.decode(encoding)
+            return content
+
         except Exception as e:
             print(e)
             print(url)
