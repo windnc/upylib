ba0ae69c4cbf833fa44a394024c242b889e49929
windnc <windnc@gmail.com>
2018-12-18 16:27:47 +0900

    fi to fn

--- upylib/util/image.py
+++ upylib/util/image.py
@@ -2,20 +2,20 @@ from PIL import Image
 from PIL import ExifTags
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
-def save_thumb(src_fi, dst_fn, size, verbose=1):
+def save_thumb(src_fn, dst_fn, size, verbose=1):
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
+    # open
     try:
-        img = Image.open(src_fi.full_fn)
+        img = Image.open(src_fn)
     except Exception as e:
         if verbose>=1:
             print(e)
         return False
 
-    #if src_fi.ext == "jpg" or src_fi.ext == "jpeg":
+    # rotate based on exif
     try:
         exif_dict = img._getexif()
         if exif_dict:
-
-            # rotate
             orientation = None
             for k, v in ExifTags.TAGS.items() : 
                 if v == "Orientation":
@@ -31,9 +31,11 @@ def save_thumb(src_fi, dst_fn, size, verbose=1):
                 else:
                     pass
     except Exception as e:
-        #print(e)
+        if verbose>=1:
+            print(e)
         pass
 
+    # prevent alpha channel warning
     img = img.convert("RGB")
 
     # save
@@ -46,3 +48,18 @@ def save_thumb(src_fi, dst_fn, size, verbose=1):
         return False
 
     return True
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
+def test_save_thumb():
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~8
+    src_fn = "/tmp/image.jpg"
+    dst_fn = "/tmp/image_thumb.jpg"
+    size = (120, 100)
+    if save_thumb(src_fn, dst_fn, size, verbose=1):
+        print("ok")
+    else:
+        print("fail")
+
+
+test_save_thumb()
