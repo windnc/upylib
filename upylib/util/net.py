56df69f81e090cb95641640d56640e0c3f551ae2
windnc <windnc@gmail.com>
2018-12-28 17:00:54 +0900

    get/post

--- upylib/util/net.py
+++ upylib/util/net.py
@@ -36,6 +36,44 @@ def read_url(url, retry=1, encoding="utf-8", verbose=1):
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+def get_url(url, retry=1, encoding="utf-8", verbose=1):
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+    return read_url(url, retru, encoding, verbose)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+def post_url(url, retry=1, data_byte=None, data_dict=None, encoding="utf-8", verbose=1):
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+    if not url:
+        if verbose >= 1:
+            print("no url")
+        return False
+
+    for _ in range(0,retry):
+        try:
+            if data_byte:
+                data = data_byte
+            elif data_dict:
+                data = urllib.parse.urlencode( data_dict ).encode("ascii")
+            else:
+                data = None
+
+            c = urllib.request.urlopen(url=url, data=data)
+            content = c.read()
+            if encoding:
+                content = content.decode(encoding)
+            return content
+
+        except Exception as e:
+            if verbose >= 1:
+                print(e)
+            continue
+
+    return False
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 def urlenc(q):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
     return urllib.parse.quote_plus(q)
