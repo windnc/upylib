f636ac42b21b562a359ec2605a56544515f58127
windnc <windnc@gmail.com>
2019-01-03 13:19:20 +0900

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(name='upylib',
       packages=find_packages(),
-      version='0.4.20',
+      version='0.4.21',
       url='https://gitlab.com',
       author='windnc',
       author_email='windnc@gmail.com',
